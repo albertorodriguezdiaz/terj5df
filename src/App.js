86451ab777ff7f502a5d76ac42bb4a0a37084583
albertorodriguezdiaz <alberto@efectovirtual.com>
2021-11-16 10:23:49 -0500

    Agregar invitados a lista otra version

--- src/App.js
+++ src/App.js
@@ -1,54 +1,50 @@
-import React, {useState} from 'react';
+import React, { Component } from 'react';
 
-function App() {
-
-
-  const [ invitado, guadarInvitado] = useState({});
-
-  const [ invitados, guardarIncitados] = useState([]);
-
-
-  const handleChange = e => {
-    guadarInvitado({
-        ...invitado,
-        [e.target.name] : e.target.value
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      list: [],
+      newName: '',
+      newLastName:''
+    }
+  }
+  nameChange(e){
+    this.setState({
+      newName: e.target.value
     })
   }
-
-  const agregarInvitado = (e) =>{
-    e.preventDefault();
-    guardarIncitados([...invitados, invitado]);
+  lastNameChange(e){
+    this.setState({
+      newLastName: e.target.value
+    })
   }
-
-  const nombre = 'first-name'; 
-  const apellido = 'last-name'; 
-
+  enterHandle(e){
+    e.preventDefault();
+    this.setState( prevState => ({
+      list: [...prevState.list, {id: this.state.list.length+1, name: this.state.newName, lastname: this.state.newLastName}],
+      newName: '',
+      newLastName: ''
+    }))
+  }
+  render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form
-              onSubmit={agregarInvitado}
-            >
+            <form onSubmit={this.enterHandle.bind(this)}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" 
-                  onChange={handleChange}
-                />
+                <input type="text" className="form-control" name="first-name" value={this.state.newName} onChange={this.nameChange.bind(this)}/>
               </div>
-
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name"
-                  onChange={handleChange}
-                 />
+                <input type="text" className="form-control" name="last-name" value={this.state.newLastName} onChange={this.lastNameChange.bind(this)}/>
               </div>
-
               <div className="action">
                 <button type="submit" className="btn btn-primary">Agregar Invitado</button>
               </div>
             </form>
-
             <table className="table bordered-table table-striped">
               <thead>
                 <tr>
@@ -57,22 +53,19 @@ function App() {
                 </tr>
               </thead>
               <tbody>
-                  { 
-                    invitados.map((list)=>
-                      <tr>
-                        <td>{list[nombre]}</td>
-                        <td>{list[apellido]}</td>
-                      </tr>
-                    )
-                  }
+              {this.state.list.map( (guest, index) =>
+                <tr key={index}>
+                  <td>{guest.name}</td>
+                  <td>{guest.lastname}</td>
+                </tr>
+              )}
               </tbody>
             </table>
           </div>
         </div>
       </div>
     )
+  }
 }
 
 export default App;
-
-
